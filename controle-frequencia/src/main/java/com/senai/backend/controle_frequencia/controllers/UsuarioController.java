78c65c0eddcbc5d479f2026b8bdd52c4e4af7aa6
rdefreitasvenancio-create <rdefreitasvenancio@gmail.com>
2026-04-17 15:27:37 -0300

    usuario controller

--- controle-frequencia/src/main/java/com/senai/backend/controle_frequencia/controllers/UsuarioController.java
+++ controle-frequencia/src/main/java/com/senai/backend/controle_frequencia/controllers/UsuarioController.java
@@ -1,15 +1,59 @@
 package com.senai.backend.controle_frequencia.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senai.backend.controle_frequencia.models.Usuario;
 import com.senai.backend.controle_frequencia.services.UsuarioService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping("/contar-usuarios")
+    public Long contarUsuarios() {
+        return usuarioService.contarUsuario();
+    }
+    @GetMapping("/buscar-usuario/{id}")
+    public Usuario buscarUsuario(@PathVariable Integer id){
+        return usuarioService.buscarUsuario(id);
+    }
+    @GetMapping("/listar-usuarios")
+    public List<Usuario> listarUsuarios(){
+        return usuarioService.listarUsuarios();
+    }
+    @DeleteMapping("/deletar-usuario/{id}")
+    public String deletarUsuario(@PathVariable Integer id){
+        if(usuarioService.deletarUsuario(id)){
+            return "Usuario deletado com sucesso";
+        }
+        return "Falha ao deletar o usuário";
+    }
+    @PostMapping("/salvar-usuario")
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario){
+        return usuarioService.cadastrarUsuario(usuario);
+    }
+    @PutMapping("/atualizar-usuario/{id}")
+    public String atualizarUsuario(@PathVariable Integer id, @RequestBody Usuario usuario){
+        if(usuarioService.atualizarUsuario(id, usuario) !=null){
+            return "Usuário atualizado com sucesso";
+        }
+        return "Falha ao atualizar o usuário";
+    }
     
 }
